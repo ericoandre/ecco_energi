2e78340c9d4e169c1a72f51e19c2ccebf8439ce2
ericoandre <ericoandresilva@gmail.com>
2023-06-04 18:22:44 -0300

    inicial

--- lib/pages/usercreat_page.dart
+++ lib/pages/usercreat_page.dart
@@ -50,20 +50,28 @@ class _UserCreatPageState extends State<UserCreatPage> {
                     obscureText: true,
                   ),
                   const SizedBox(height: 10.0),
-                  SizedBox(
-                      width: double.infinity,
-                      height: 50,
-                      child: CustomButton(
-                        onPressed: () async {
-                          Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const LoginPage()));
-                        },
-                        text: "Criar Cadastro",
-                        backColor: Colors.blue,
-                        textColor: Colors.white,
-                      ))
+                  Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 25, horizontal: 35),
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                                width: double.infinity,
+                                height: 50,
+                                child: CustomButton(
+                                  onPressed: () async {
+                                    Navigator.pushReplacement(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                const LoginPage()));
+                                  },
+                                  text: "Criar Cadastro",
+                                  backColor: Colors.blue,
+                                  textColor: Colors.white,
+                                ))
+                          ]))
                 ])));
   }
 }
